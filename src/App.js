fcfed3891908c94fb72c219dd8e479b95587789a
Dario Hernandez <dario.hernandez@sanoma.com>
2024-10-26 18:40:41 +0200

    Install Tailwind CSS and some initial changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Header from './components/header.js';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <p className="text-4xl font-bold text-blue-600">¡Este es mi portafolio personal!</p>
     </div>
   );
 }
---
+++ src/components/header.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Header() {
+  return <h1 className="text-4xl font-bold text-blue-600">Bienvenido a Mi Portafolio</h1>;
+}
+
+export default Header;
